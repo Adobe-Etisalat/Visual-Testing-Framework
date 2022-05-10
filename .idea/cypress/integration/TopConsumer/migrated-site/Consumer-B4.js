81be3a6515b8f8971a35a4b17001c4c73451066a
ANITHA PARUCHURI <cap24213@adobe.com>
2022-05-10 18:07:38 +0530

    topconsumer testdata batch files updtaed

--- .idea/cypress/integration/TopConsumer/migrated-site/Consumer-B4.js
+++ .idea/cypress/integration/TopConsumer/migrated-site/Consumer-B4.js
@@ -15,7 +15,7 @@ describe("visual test of walls site page",function(){
 
     beforeEach(function()
     {
-        cy.fixture("/TopConsumer/migrated/Consumer-B1").then((data) =>{
+        cy.fixture("/TopConsumer/migrated/Consumer-B4").then((data) =>{
             this.testdata=data
         }) 
     })
@@ -39,7 +39,7 @@ describe("visual test of walls site page",function(){
              cy.window().then(cyWindow => scrollToBottom({frequency: 10, timing: 5000 ,remoteWindow: cyWindow }));
          i=i+1
          cy.log(i)
-       cy.percySnapshot("Top-Consumer-batch1-page-"+i);
+       cy.percySnapshot("Top-Consumer-batch4-page-"+i);
         });
        })
 }) 
