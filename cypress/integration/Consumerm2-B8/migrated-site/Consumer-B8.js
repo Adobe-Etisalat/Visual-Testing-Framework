f8efabd3d15240ef7bab9af9d4dd0e511892dbb8
ANITHA PARUCHURI <cap24213@adobe.com>
2022-06-30 14:11:13 +0530

    Consumer module -B8

--- cypress/integration/Consumerm2-B8/migrated-site/Consumer-B8.js
+++ cypress/integration/Consumerm2-B8/migrated-site/Consumer-B8.js
@@ -15,7 +15,7 @@ describe("visual test of walls site page",function(){
 
     beforeEach(function()
     {
-        cy.fixture("/Consumerm2-B7/migrated/Consumer-B7").then((data) =>{
+        cy.fixture("/Consumerm2-B8/migrated/Consumer-B8").then((data) =>{
             this.testdata=data
         }) 
     })
@@ -39,7 +39,7 @@ describe("visual test of walls site page",function(){
              cy.window().then(cyWindow => scrollToBottom({frequency: 10, timing: 5000 ,remoteWindow: cyWindow }));
          i=i+1
          cy.log(i)
-       cy.percySnapshot("Consumerm2-bat7-page-"+i);
+       cy.percySnapshot("Consumerm2-bat8-page-"+i);
         });
        })
 }) 
