916e0c654f83aa455c08f22d1ab54f1093732bcb
ANITHA PARUCHURI <cap24213@adobe.com>
2022-07-01 22:26:29 +0530

    dynamic pages updated

--- cypress/integration/dynamic/migrated-site/dynamic-7.js
+++ cypress/integration/dynamic/migrated-site/dynamic-7.js
@@ -39,7 +39,7 @@ describe("visual test of walls site page",function(){
              cy.window().then(cyWindow => scrollToBottom({frequency: 10, timing: 5000 ,remoteWindow: cyWindow }));
          i=i+1
          cy.log(i)
-       cy.percySnapshot("dynamic7-page-"+i);
+       cy.percySnapshot("dynamicpages7-page-"+i);
         });
        })
 }) 
--- cypress/integration/dynamic/prod-site/dynamic-1.js
+++ cypress/integration/dynamic/prod-site/dynamic-1.js
@@ -39,7 +39,7 @@ describe("visual test of walls site page",function(){
              cy.window().then(cyWindow => scrollToBottom({frequency: 10, timing: 5000 ,remoteWindow: cyWindow }));
          i=i+1
          cy.log(i)
-       cy.percySnapshot("dynamic1-page-"+i);
+       cy.percySnapshot("dynamicpages1-page-"+i);
         });
        })
 }) 
