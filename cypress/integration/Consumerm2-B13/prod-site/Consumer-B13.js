41866bb3184da2601bba940b892bef65f8b0ddc8
ANITHA PARUCHURI <cap24213@adobe.com>
2022-07-01 16:02:21 +0530

    Consumer mod B13

--- cypress/integration/Consumerm2-B13/prod-site/Consumer-B13.js
+++ cypress/integration/Consumerm2-B13/prod-site/Consumer-B13.js
@@ -15,7 +15,7 @@ describe("visual test of walls site page",function(){
 
     beforeEach(function()
     {
-        cy.fixture("/Consumerm2-B12/migrated/Consumer-B12").then((data) =>{
+        cy.fixture("/Consumerm2-B13/production/Consumer-B13").then((data) =>{
             this.testdata=data
         }) 
     })
@@ -39,7 +39,7 @@ describe("visual test of walls site page",function(){
              cy.window().then(cyWindow => scrollToBottom({frequency: 10, timing: 5000 ,remoteWindow: cyWindow }));
          i=i+1
          cy.log(i)
-       cy.percySnapshot("Consumerm2-bat12-page-"+i);
+       cy.percySnapshot("Consumerm2-bat13-page-"+i);
         });
        })
 }) 
